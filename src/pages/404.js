ae8a3b7b5f7b27d30079a456d9f5864662811ef1
Hannah Chea <cheahannah@berkeley.edu>
2020-06-01 07:53:57 -0700

    Added 404 error page

---
+++ src/pages/404.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import PageLayout from "../components/PageLayout";
+import "./about.css";
+
+const ErrorPage = () => (
+    <div>
+        <Navbar />
+        <PageLayout title="404" width={30} children={"Education", "Certifications"}>
+            <div>
+            <p>Oops! Page not found :(</p>
+            <p></p>
+            <p></p>
+            </div>
+        </PageLayout>
+        <Footer />
+    </div>
+);
+
+export default ErrorPage;
