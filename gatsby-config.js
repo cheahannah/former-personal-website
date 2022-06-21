1ad9c8a3028af892c94593ad2b07d44df6fe55d6
Hannah Chea <cheahannah@berkeley.edu>
2022-06-20 20:39:04 -0700

    First commit of new website!

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,11 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://cheahannah.me/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
+          siteName: "Hannah Chea Personal Website", // Used in manifest.json
+          shortName: "Hannah Chea", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
@@ -18,11 +18,11 @@ module.exports = {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
+         googleAnalytics: {
+             trackingId: "320161938",
+             anonymize: true, // Default true
+             environments: ["production", "development"] // Default ["production"]
+         }
       },
     },
   ],
